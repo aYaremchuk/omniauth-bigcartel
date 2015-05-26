527128965279ba441180aa50aabe57f6a31e6db3
Andrey Yaremchuk <andrey.yaremchuk@softbistro.com>
2015-05-26 19:13:30 +0300

    Add uid and info

--- lib/omniauth/strategies/bigcartel.rb
+++ lib/omniauth/strategies/bigcartel.rb
@@ -5,18 +5,21 @@ module OmniAuth
     class Bigcartel < OmniAuth::Strategies::OAuth2
       option :name, :bigcartel
 
-      option :client_options, site: 'https://my.bigcartel.com'
-
+      option :client_options, {
+                                site: 'https://api.bigcartel.com',
+                                authorize_url: 'https://my.bigcartel.com/oauth/authorize',
+                                token_url: 'https://api.bigcartel.com/oauth/token'
+                            }
 
       uid do
-        access_token.params['store']['id']
+        access_token.params['account_id']
       end
 
       info do
         {
-            name: access_token.params['store']['name'],
-            email: access_token.params['store']['storekeeper_email'],
-            url: access_token.params['store']['url']
+            name: access_token.params['store_name'],
+            email: access_token.params['contact_email'],
+            url: access_token.params['url']
         }
       end
     end
